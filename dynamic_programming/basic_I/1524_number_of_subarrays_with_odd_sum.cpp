b8ca0b18138ff3f7b931143dfce542d664b832ca
mirinta <mirinta@163.com>
2025-02-25 10:24:06 +0800

    Update 1524_number_of_subarrays_with_odd_sum.cpp

--- dynamic_programming/basic_I/1524_number_of_subarrays_with_odd_sum.cpp
+++ dynamic_programming/basic_I/1524_number_of_subarrays_with_odd_sum.cpp
@@ -18,40 +18,62 @@ public:
 private:
     static constexpr int kMod = 1e9 + 7;
 
-    // DP with space optimization, TC = O(N), SC = O(1)
+    // Prefix sum and counting, TC = O(N), SC = O(1)
+    int approach3(const std::vector<int>& arr)
+    {
+        int sum = 0;
+        int odd = 0;
+        int even = 0;
+        int result = 0;
+        for (const auto& val : arr) {
+            sum += val;
+            if (sum % 2) {
+                odd++;
+                result = (result + even) % kMod;
+                result = (result + 1) % kMod;
+            } else {
+                even++;
+                result = (result + odd) % kMod;
+            }
+        }
+        return result;
+    }
+
+    // DP with space optimization, TC = O(N), SC = O(N)
     int approach2(const std::vector<int>& arr)
     {
         const int n = arr.size();
-        int odd = arr[0] & 1;
-        int even = 1 - odd;
+        int odd = arr[0] % 2;
+        int even = 1 - arr[0] % 2;
         int result = odd;
         for (int i = 1; i < n; ++i) {
-            if (arr[i] & 1) {
-                std::swap(odd, even);
-                odd++;
+            if (arr[i] % 2) {
+                const int copyOdd = odd;
+                odd = (even + 1) % kMod;
+                even = copyOdd;
             } else {
-                even++;
+                even = (even + 1) % kMod;
             }
             result = (result + odd) % kMod;
         }
         return result;
     }
 
-    // DP, TC = O(N), SC = O(N)
+    // DP, TC = O(N), SC = O(1)
     int approach1(const std::vector<int>& arr)
     {
         // dp[i][0] = num of subarrays of nums[0:i] ending at nums[i] with an even sum
         // dp[i][1] = num of subarrays of nums[0:i] ending at nums[i] with an odd sum
         const int n = arr.size();
         std::vector<std::array<int, 2>> dp(n, {0, 0});
-        dp[0][arr[0] & 1] = 1;
+        dp[0][arr[0] % 2] = 1;
         int result = dp[0][1];
         for (int i = 1; i < n; ++i) {
-            if (arr[i] & 1) {
+            if (arr[i] % 2) {
                 dp[i][0] = dp[i - 1][1];
-                dp[i][1] = 1 + dp[i - 1][0];
+                dp[i][1] = (dp[i - 1][0] + 1) % kMod;
             } else {
-                dp[i][0] = 1 + dp[i - 1][0];
+                dp[i][0] = (dp[i - 1][0] + 1) % kMod;
                 dp[i][1] = dp[i - 1][1];
             }
             result = (result + dp[i][1]) % kMod;
