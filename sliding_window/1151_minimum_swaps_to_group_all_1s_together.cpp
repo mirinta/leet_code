8d10550b823374f81e4ea39d2ae53291ab33becb
mirinta <mirinta@163.com>
2025-03-03 16:12:43 +0800

    Create 1151_minimum_swaps_to_group_all_1s_together.cpp

---
+++ sliding_window/1151_minimum_swaps_to_group_all_1s_together.cpp
@@ -0,0 +1,30 @@
+#include <climits>
+#include <numeric>
+#include <vector>
+
+/**
+ * Given a binary array data, return the minimum number of swaps required to group all 1’s present
+ * in the array together in any place in the array.
+ *
+ * ! 1 <= data.length <= 10^5
+ * ! data[i] is either 0 or 1.
+ */
+
+class Solution
+{
+public:
+    int minSwaps(std::vector<int>& data)
+    {
+        const int n = data.size();
+        const int totalOnes = std::accumulate(data.begin(), data.end(), 0);
+        int result = INT_MAX;
+        for (int left = 0, right = 0, sum = 0; right < n; ++right) {
+            sum += data[right];
+            if (right >= totalOnes) {
+                sum -= data[left++];
+            }
+            result = std::min(result, totalOnes - sum);
+        }
+        return result;
+    }
+};
