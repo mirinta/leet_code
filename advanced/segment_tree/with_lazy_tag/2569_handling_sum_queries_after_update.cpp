5b534e47f3a13c87c019c50b28d67960d17776da
mirinta <mirinta@163.com>
2025-03-17 11:33:45 +0800

    Update 2569_handling_sum_queries_after_update.cpp

--- advanced/segment_tree/with_lazy_tag/2569_handling_sum_queries_after_update.cpp
+++ advanced/segment_tree/with_lazy_tag/2569_handling_sum_queries_after_update.cpp
@@ -25,74 +25,103 @@
  * ! 0 <= nums2[i] <= 10^9
  */
 
+class SegmentTree
+{
+public:
+    explicit SegmentTree(const std::vector<int>& nums)
+        : n(nums.size()), data(4 * n + 1, 0), lazy(4 * n + 1, 0)
+    {
+        build(0, n - 1, 1, nums);
+    }
+
+    long long query(int L, int R) { return query(L, R, 0, n - 1, 1); }
+
+    void flip(int L, int R) { flip(L, R, 0, n - 1, 1); }
+
+private:
+    void build(int lo, int hi, int id, const std::vector<int>& nums)
+    {
+        if (lo == hi) {
+            data[id] = nums[lo];
+            return;
+        }
+        const int mid = lo + (hi - lo) / 2;
+        build(lo, mid, 2 * id, nums);
+        build(mid + 1, hi, 2 * id + 1, nums);
+        data[id] = data[2 * id] + data[2 * id + 1];
+    }
+
+    long long query(int L, int R, int lo, int hi, int id)
+    {
+        if (lo >= L && hi <= R)
+            return data[id];
+
+        const int mid = lo + (hi - lo) / 2;
+        if (lazy[id]) {
+            data[2 * id] = (mid - lo + 1) - data[2 * id];
+            data[2 * id + 1] = (hi - mid) - data[2 * id + 1];
+            lazy[2 * id] = !lazy[2 * id];
+            lazy[2 * id + 1] = !lazy[2 * id + 1];
+            lazy[id] = false;
+        }
+        long long result = 0;
+        if (mid >= L) {
+            result += query(L, R, lo, mid, 2 * id);
+        }
+        if (mid < R) {
+            result += query(L, R, mid + 1, hi, 2 * id + 1);
+        }
+        return result;
+    }
+
+    void flip(int L, int R, int lo, int hi, int id)
+    {
+        if (lo >= L && hi <= R) {
+            data[id] = (hi - lo + 1) - data[id];
+            lazy[id] = !lazy[id];
+            return;
+        }
+        const int mid = lo + (hi - lo) / 2;
+        if (lazy[id]) {
+            data[2 * id] = (mid - lo + 1) - data[2 * id];
+            data[2 * id + 1] = (hi - mid) - data[2 * id + 1];
+            lazy[2 * id] = !lazy[2 * id];
+            lazy[2 * id + 1] = !lazy[2 * id + 1];
+            lazy[id] = false;
+        }
+        if (mid >= L) {
+            flip(L, R, lo, mid, 2 * id);
+        }
+        if (mid < R) {
+            flip(L, R, mid + 1, hi, 2 * id + 1);
+        }
+        data[id] = data[2 * id] + data[2 * id + 1];
+    }
+
+private:
+    const int n;
+    std::vector<long long> data;
+    std::vector<bool> lazy;
+};
+
 class Solution
 {
 public:
     std::vector<long long> handleQuery(std::vector<int>& nums1, std::vector<int>& nums2,
                                        std::vector<std::vector<int>>& queries)
     {
-        const int n = nums1.size();
+        SegmentTree tree(nums1);
         long long sum = std::accumulate(nums2.begin(), nums2.end(), 0LL);
-        data = std::vector<long long>(4 * n, 0);
-        todo = std::vector<bool>(4 * n, false);
-        build(1, 1, n, nums1);
         std::vector<long long> result;
         for (const auto& q : queries) {
             if (q[0] == 1) {
-                const int L = q[1] + 1; // to 1-indexed
-                const int R = q[2] + 1; // to 1-indexed
-                update(1, 1, n, L, R);
+                tree.flip(q[1], q[2]);
             } else if (q[0] == 2) {
-                sum += data[1] * q[1];
+                sum += tree.query(0, nums1.size() - 1) * q[1];
             } else {
                 result.push_back(sum);
             }
         }
         return result;
-    }
-
-private:
-    std::vector<long long> data; // data[i] = num of 1's of the ith range of nums1, 1-indexed
-    std::vector<bool> todo;      // lazy tags, do flip or not, 1-indexed
-
-    void build(int id, int lo, int hi, const std::vector<int>& nums1)
-    {
-        if (lo == hi) {
-            data[id] = nums1[lo - 1];
-            return;
-        }
-        const int mid = lo + (hi - lo) / 2;
-        build(2 * id, lo, mid, nums1);
-        build(2 * id + 1, mid + 1, hi, nums1);
-        data[id] = data[2 * id] + data[2 * id + 1];
-    }
-
-    void flip(int id, int lo, int hi)
-    {
-        // flip [lo,hi], all 1's change to 0's
-        data[id] = hi - lo + 1 - data[id];
-        todo[id] = !todo[id];
-    }
-
-    void update(int id, int lo, int hi, int L, int R)
-    {
-        // L ... lo ... hi ... R
-        if (lo >= L && hi <= R) {
-            flip(id, lo, hi);
-            return;
-        }
-        const int mid = lo + (hi - lo) / 2;
-        if (todo[id]) {
-            flip(2 * id, lo, mid);
-            flip(2 * id + 1, mid + 1, hi);
-            todo[id] = false;
-        }
-        if (mid >= L) {
-            update(2 * id, lo, mid, L, R);
-        }
-        if (mid + 1 <= R) {
-            update(2 * id + 1, mid + 1, hi, L, R);
-        }
-        data[id] = data[2 * id] + data[2 * id + 1];
     }
 };
