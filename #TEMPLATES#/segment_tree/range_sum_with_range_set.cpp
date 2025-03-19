69c54a54fb238d213e96da951330fe0c51f0cd2e
mirinta <mirinta@163.com>
2025-03-19 18:00:52 +0800

    Update range_sum_with_range_set.cpp

--- #TEMPLATES#/segment_tree/range_sum_with_range_set.cpp
+++ #TEMPLATES#/segment_tree/range_sum_with_range_set.cpp
@@ -10,30 +10,30 @@ public:
     }
 
     // query sum of nums[L:R], L and R are 0-indexed
-    long long query(int L, int R) { return query(L, R, 0, n - 1, 1); }
+    long long query(long long L, long long R) { return query(L, R, 0, n - 1, 1); }
 
     // set all elements of nums[L:R] to val, L and R are 0-indexed
-    void set(int L, int R, long long val) { set(L, R, val, 0, n - 1, 1); }
+    void set(long long L, long long R, long long val) { set(L, R, val, 0, n - 1, 1); }
 
 private:
-    void build(int lo, int hi, int id, const std::vector<long long>& nums)
+    void build(long long lo, long long hi, long long id, const std::vector<long long>& nums)
     {
         if (lo == hi) {
             data[id] = nums[lo];
             return;
         }
-        const int mid = lo + (hi - lo) / 2;
+        const long long mid = lo + (hi - lo) / 2;
         build(lo, mid, 2 * id, nums);
         build(mid + 1, hi, 2 * id + 1, nums);
         data[id] = data[2 * id] + data[2 * id + 1];
     }
 
-    long long query(int L, int R, int lo, int hi, int id)
+    long long query(long long L, long long R, long long lo, long long hi, long long id)
     {
         if (lo >= L && hi <= R)
             return data[id];
 
-        const int mid = lo + (hi - lo) / 2;
+        const long long mid = lo + (hi - lo) / 2;
         if (flag[id]) {
             data[2 * id] = lazy[id] * (mid - lo + 1);
             data[2 * id + 1] = lazy[id] * (hi - mid);
@@ -53,7 +53,7 @@ private:
         return result;
     }
 
-    void set(int L, int R, long long val, int lo, int hi, int id)
+    void set(long long L, long long R, long long val, long long lo, long long hi, long long id)
     {
         if (lo >= L && hi <= R) {
             data[id] = val * (hi - lo + 1);
@@ -61,7 +61,7 @@ private:
             flag[id] = true;
             return;
         }
-        const int mid = lo + (hi - lo) / 2;
+        const long long mid = lo + (hi - lo) / 2;
         if (flag[id]) {
             data[2 * id] = lazy[id] * (mid - lo + 1);
             data[2 * id + 1] = lazy[id] * (hi - mid);
@@ -81,7 +81,7 @@ private:
     }
 
 private:
-    const int n;
+    const long long n;
     std::vector<long long> data; // 1-indexed
     std::vector<long long> lazy; // 1-indexed
     std::vector<bool> flag;      // 1-indexed
