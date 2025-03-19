e4539dea957d5848d8c73a855af401971d73590b
mirinta <mirinta@163.com>
2025-03-19 17:59:21 +0800

    Update range_max_with_range_set.cpp

--- #TEMPLATES#/segment_tree/range_max_with_range_set.cpp
+++ #TEMPLATES#/segment_tree/range_max_with_range_set.cpp
@@ -11,25 +11,25 @@ public:
     }
 
     // query the max element of nums[L:R], L and R are 0-indexed
-    int query(int L, int R) { return query(L, R, 0, n - 1, 1); }
+    long long query(long long L, long long R) { return query(L, R, 0, n - 1, 1); }
 
     // set all elements of nums[L:R] to val, L and R are 0-indexed
-    void set(int L, int R, int val) { set(L, R, val, 0, n - 1, 1); }
+    void set(long long L, long long R, long long val) { set(L, R, val, 0, n - 1, 1); }
 
 private:
-    void build(int lo, int hi, int id, const std::vector<long long>& nums)
+    void build(long long lo, long long hi, long long id, const std::vector<long long>& nums)
     {
         if (lo == hi) {
             data[id] = nums[lo];
             return;
         }
-        const int mid = lo + (hi - lo) / 2;
+        const long long mid = lo + (hi - lo) / 2;
         build(lo, mid, 2 * id, nums);
         build(mid + 1, hi, 2 * id + 1, nums);
         data[id] = std::max(data[2 * id], data[2 * id + 1]);
     }
 
-    long long query(int L, int R, int lo, int hi, int id)
+    long long query(long long L, long long R, long long lo, long long hi, long long id)
     {
         if (lo >= L && hi <= R)
             return data[id];
@@ -44,8 +44,8 @@ private:
             flag[2 * id + 1] = true;
             flag[id] = false;
         }
-        const int mid = lo + (hi - lo) / 2;
-        long long result = INT_MIN;
+        const long long mid = lo + (hi - lo) / 2;
+        long long result = LLONG_MIN;
         if (mid >= L) {
             result = std::max(result, query(L, R, lo, mid, 2 * id));
         }
@@ -55,7 +55,7 @@ private:
         return result;
     }
 
-    void set(int L, int R, int val, int lo, int hi, int id)
+    void set(long long L, long long R, long long val, long long lo, long long hi, long long id)
     {
         if (lo >= L && hi <= R) {
             data[id] = val;
@@ -73,7 +73,7 @@ private:
             flag[2 * id + 1] = true;
             flag[id] = false;
         }
-        const int mid = lo + (hi - lo) / 2;
+        const long long mid = lo + (hi - lo) / 2;
         if (mid >= L) {
             set(L, R, val, lo, mid, 2 * id);
         }
@@ -84,7 +84,7 @@ private:
     }
 
 private:
-    const int n;
+    const long long n;
     std::vector<long long> data; // 1-indexed
     std::vector<long long> lazy; // 1-indexed
     std::vector<bool> flag;      // 1-indexed
