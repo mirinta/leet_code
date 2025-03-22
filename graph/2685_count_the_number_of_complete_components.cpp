ed277c3c75f8c81cbd0cd2bc2bd0ecc18fe24253
mirinta <mirinta@163.com>
2025-03-22 13:12:29 +0800

    solutions

---
+++ graph/2685_count_the_number_of_complete_components.cpp
@@ -0,0 +1,83 @@
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+
+/**
+ * You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n
+ * - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists
+ * an undirected edge connecting vertices ai and bi.
+ *
+ * Return the number of complete connected components of the graph.
+ *
+ * A connected component is a subgraph of a graph in which there exists a path between any two
+ * vertices, and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+ *
+ * A connected component is said to be complete if there exists an edge between every pair of its
+ * vertices.
+ *
+ * ! 1 <= n <= 50
+ * ! 0 <= edges.length <= n * (n - 1) / 2
+ * ! edges[i].length == 2
+ * ! 0 <= ai, bi <= n - 1
+ * ! ai != bi
+ * ! There are no repeated edges.
+ */
+
+class UnionFind
+{
+public:
+    explicit UnionFind(int n) : root(n), size(n, 1) { std::iota(root.begin(), root.end(), 0); }
+
+    int find(int x)
+    {
+        if (x != root[x]) {
+            root[x] = find(root[x]);
+        }
+        return root[x];
+    }
+
+    void connect(int p, int q)
+    {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+
+        if (size[rootQ] > size[rootP]) {
+            std::swap(rootP, rootQ);
+        }
+        root[rootQ] = rootP;
+        size[rootP] += size[rootQ];
+    }
+
+    int sizeOfGroup(int x) { return size[find(x)]; }
+
+private:
+    std::vector<int> root;
+    std::vector<int> size;
+};
+
+class Solution
+{
+public:
+    int countCompleteComponents(int n, std::vector<std::vector<int>>& edges)
+    {
+        UnionFind uf(n);
+        for (const auto& e : edges) {
+            uf.connect(e[0], e[1]);
+        }
+        std::unordered_map<int, int> map;
+        for (const auto& e : edges) {
+            map[uf.find(e[0])]++;
+        }
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != uf.find(i))
+                continue;
+
+            const int m = uf.sizeOfGroup(i);
+            result += map[uf.find(i)] == m * (m - 1) / 2;
+        }
+        return result;
+    }
+};
---
+++ graph/shortest_path/2737_find_the_closet_marked_node.cpp
@@ -0,0 +1,64 @@
+#include <queue>
+#include <unordered_set>
+#include <vector>
+
+/**
+ * You are given a positive integer n which is the number of nodes of a 0-indexed directed weighted
+ * graph and a 0-indexed 2D array edges where edges[i] = [ui, vi, wi] indicates that there is an
+ * edge from node ui to node vi with weight wi.
+ *
+ * You are also given a node s and a node array marked; your task is to find the minimum distance
+ * from s to any of the nodes in marked.
+ *
+ * Return an integer denoting the minimum distance from s to any node in marked or -1 if there are
+ * no paths from s to any of the marked nodes.
+ *
+ * ! 2 <= n <= 500
+ * ! 1 <= edges.length <= 10^4
+ * ! edges[i].length = 3
+ * ! 0 <= edges[i][0], edges[i][1] <= n - 1
+ * ! 1 <= edges[i][2] <= 10^6
+ * ! 1 <= marked.length <= n - 1
+ * ! 0 <= s, marked[i] <= n - 1
+ * ! s != marked[i]
+ * ! marked[i] != marked[j] for every i != j
+ * ! The graph might have repeated edges.
+ * ! The graph is generated such that it has no self-loops.
+ */
+
+class Solution
+{
+public:
+    int minimumDistance(int n, std::vector<std::vector<int>>& edges, int s,
+                        std::vector<int>& marked)
+    {
+        std::vector<std::vector<std::pair<int, int>>> graph(n);
+        for (const auto& e : edges) {
+            graph[e[0]].emplace_back(e[1], e[2]);
+        }
+        std::unordered_set<int> set(marked.begin(), marked.end());
+        std::vector<int> distTo(n, INT_MAX);
+        distTo[s] = 0;
+        using Pair = std::pair<int, int>;
+        auto compare = [](const Pair& p1, const Pair& p2) { return p1.second > p2.second; };
+        std::priority_queue<Pair, std::vector<Pair>, decltype(compare)> pq(compare);
+        pq.emplace(s, 0);
+        while (!pq.empty()) {
+            const auto [v, dist] = pq.top();
+            pq.pop();
+            if (dist > distTo[v])
+                continue;
+
+            if (set.count(v))
+                return dist;
+
+            for (const auto& [adj, weight] : graph[v]) {
+                if (distTo[adj] > dist + weight) {
+                    distTo[adj] = dist + weight;
+                    pq.emplace(adj, distTo[adj]);
+                }
+            }
+        }
+        return -1;
+    }
+};
