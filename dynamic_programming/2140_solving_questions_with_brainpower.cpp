26c0c75916e9dc2df5e782d7501ad5a4c635ce54
mirinta <mirinta@163.com>
2025-04-01 16:20:04 +0800

    Update 2140_solving_questions_with_brainpower.cpp

--- dynamic_programming/2140_solving_questions_with_brainpower.cpp
+++ dynamic_programming/2140_solving_questions_with_brainpower.cpp
@@ -1,35 +1,26 @@
 #include <vector>
 
 /**
- * You are given a 0-indexed 2D integer array "questions" where "questions[i] = [points_i,
- * brainpower_i]".
+ * You are given a 0-indexed 2D integer array questions where questions[i] = [pointsi, brainpoweri].
  *
  * The array describes the questions of an exam, where you have to process the questions in order
  * (i.e., starting from question 0) and make a decision whether to solve or skip each question.
- * Solving question i will earn you points_i points but you will be unable to solve each of the next
- * brainpower_i questions. If you skip question i, you get to make the decision on the next
- * question.
+ * Solving question i will earn you pointsi points but you will be unable to solve each of the next
+ * brainpoweri questions. If you skip question i, you get to make the decision on the next question.
+ *
+ * - For example, given questions = [[3, 2], [4, 3], [4, 4], [2, 5]]:
+ *
+ *   - If question 0 is solved, you will earn 3 points but you will be unable to solve questions 1
+ *     and 2.
+ *
+ *   - If instead, question 0 is skipped and question 1 is solved, you will earn 4 points but you
+ *     will be unable to solve questions 2 and 3.
  *
  * Return the maximum points you can earn for the exam.
  *
- * Example:
- * Input: questions = [[3, 2], [4, 3], [4, 4], [2, 5]]
- * Output: 5
- * Explanation: The maximum points can be earned by solving question 0 and 3.
- * - Solve question 0: Earn 3 points, will be unable to solve the next 2 questions
- * - Unable to solve questions 1 and 2
- * - Solve question 3: Earn 2 points
- * Total points earned: 3 + 2 = 5. There is no other way to earn 5 or more points.
- *
- * Example:
- * Input: questions = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
- * Output: 7
- * Explanation: The maximum points can be earned by solving questions 1 and 4.
- * - Skip question 0
- * - Solve question 1: Earn 2 points, will be unable to solve the next 2 questions
- * - Unable to solve questions 2 and 3
- * - Solve question 4: Earn 5 points
- * Total points earned: 2 + 5 = 7. There is no other way to earn 7 or more points.
+ * ! 1 <= questions.length <= 10^5
+ * ! questions[i].length == 2
+ * ! 1 <= pointsi, brainpoweri <= 10^5
  */
 
 class Solution
@@ -37,23 +28,22 @@ class Solution
 public:
     long long mostPoints(std::vector<std::vector<int>>& questions)
     {
-        const auto n = questions.size();
-        if (n < 1)
+        std::vector<long long> memo(questions.size(), -1);
+        return dfs(memo, 0, questions);
+    }
+
+private:
+    long long dfs(std::vector<long long>& memo, int i,
+                  const std::vector<std::vector<int>>& questions)
+    {
+        if (i >= questions.size())
             return 0;
 
-        // dp[i] = maximum points we can get by processing the questions [i, n - 1]
-        std::vector<long long> dp(n);
-        dp[n - 1] = questions[n - 1][0];
-        for (long long i = n - 2; i >= 0; --i) {
-            // case 1: skip the i-th problem, dp[i] = dp[i + 1]
-            // case 2: solve the i-th problem, dp[i] = points[i] + dp[j]
-            // - j is the index of next solvable question after the i-th question
-            // - j = i + brainpower[i] + 1
-            // - if j >= n, there is no solvable question after solving the i-th question which
-            // means dp[j] = 0
-            const auto j = i + questions[i][1] + 1;
-            dp[i] = std::max(dp[i + 1], j < n ? dp[j] + questions[i][0] : questions[i][0]);
-        }
-        return dp[0];
+        if (memo[i] != -1)
+            return memo[i];
+
+        const long long case1 = dfs(memo, i + 1, questions);
+        const long long case2 = questions[i][0] + dfs(memo, i + 1 + questions[i][1], questions);
+        return memo[i] = std::max(case1, case2);
     }
 };
