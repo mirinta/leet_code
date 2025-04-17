270dd4ffa8ac05be16410fef9417163c220baf71
mirinta <mirinta@163.com>
2025-04-17 10:41:14 +0800

    Create tuple_hash.cpp

---
+++ #TEMPLATES#/tuple_hash.cpp
@@ -0,0 +1,17 @@
+#include <tuple>
+
+namespace std {
+template <>
+struct hash<tuple<int, int, int>>
+{
+    size_t operator()(const tuple<int, int, int>& t) const
+    {
+        const auto& [a, b, c] = t;
+        size_t seed = 0;
+        seed ^= hash<int>{}(a) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        seed ^= hash<int>{}(b) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        seed ^= hash<int>{}(c) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+        return seed;
+    }
+};
+} // namespace std
