6776cab01666225d4e112701f6e59af0c296898f
mirinta <mirinta@163.com>
2025-04-23 10:49:27 +0800

    Create 1399_count_largest_group.cpp

---
+++ math/1399_count_largest_group.cpp
@@ -0,0 +1,42 @@
+#include <unordered_map>
+
+/**
+ * You are given an integer n.
+ *
+ * Each number from 1 to n is grouped according to the sum of its digits.
+ *
+ * Return the number of groups that have the largest size.
+ *
+ * ! 1 <= n <= 10^4
+ */
+
+class Solution
+{
+public:
+    int countLargestGroup(int n)
+    {
+        std::unordered_map<int, int> map;
+        int maxSize = 0;
+        for (int i = 1; i <= n; ++i) {
+            const int sum = helper(i);
+            map[sum]++;
+            maxSize = std::max(maxSize, map[sum]);
+        }
+        int result = 0;
+        for (const auto& [sum, count] : map) {
+            result += count == maxSize;
+        }
+        return result;
+    }
+
+private:
+    int helper(int x)
+    {
+        int sum = 0;
+        while (x) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+};
