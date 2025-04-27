a8cec0d7678d1802e4b680379621ec866f192f53
mirinta <mirinta@163.com>
2025-04-27 16:00:30 +0800

    Update 2444_count_subarrays_with_fixed_bounds.cpp

--- two_pointers/same_direction/2444_count_subarrays_with_fixed_bounds.cpp
+++ two_pointers/same_direction/2444_count_subarrays_with_fixed_bounds.cpp
@@ -17,27 +17,30 @@
  * ! 1 <= nums[i], minK, maxK <= 10^6
  */
 
+#include <deque>
+#include <vector>
+
 class Solution
 {
 public:
     long long countSubarrays(std::vector<int>& nums, int minK, int maxK)
     {
-        // use elements < minK or > maxK as boundaries
-        // ... X X+1 ... lastMin ... lastMax ... i
-        //     ^
-        //     lastBoundary
-        //
-        // num of valid subarrays ending at nums[i]:
-        // nums[X+1:i], nums[X+2:i], ..., nums[lastMin:i]
-        // thus, count = std::max(0, std::min(lastMin, lastMax) - lastBoundary)
+        return approach2(nums, minK, maxK);
+    }
+
+private:
+    long long approach2(const std::vector<int>& nums, int minK, int maxK)
+    {
         const int n = nums.size();
-        long long result = 0;
         int lastMin = -1;
         int lastMax = -1;
-        int lastBoundary = -1;
+        int lastInvalid = -1;
+        long long result = 0;
         for (int i = 0; i < n; ++i) {
             if (nums[i] < minK || nums[i] > maxK) {
-                lastBoundary = i;
+                lastInvalid = i;
+                lastMin = -1;
+                lastMax = -1;
                 continue;
             }
             if (nums[i] == minK) {
@@ -46,7 +49,35 @@ public:
             if (nums[i] == maxK) {
                 lastMax = i;
             }
-            result += std::max(0, std::min(lastMin, lastMax) - lastBoundary);
+            result += std::max(0, std::min(lastMin, lastMax) - lastInvalid);
+        }
+        return result;
+    }
+
+    long long approach1(const std::vector<int>& nums, int minK, int maxK)
+    {
+        const int n = nums.size();
+        std::deque<int> minDq; // front is the index of the min element
+        std::deque<int> maxDq; // front is the index of the max element
+        long long result = 0;
+        for (int left = 0, i = 0; i < n; ++i) {
+            if (nums[i] < minK || nums[i] > maxK) {
+                minDq.clear();
+                maxDq.clear();
+                left = i + 1;
+                continue;
+            }
+            while (!minDq.empty() && nums[i] <= nums[minDq.back()]) {
+                minDq.pop_back();
+            }
+            minDq.push_back(i);
+            while (!maxDq.empty() && nums[i] >= nums[maxDq.back()]) {
+                maxDq.pop_back();
+            }
+            maxDq.push_back(i);
+            if (nums[minDq.front()] == minK && nums[maxDq.front()] == maxK) {
+                result += std::min(minDq.front(), maxDq.front()) - left + 1;
+            }
         }
         return result;
     }
