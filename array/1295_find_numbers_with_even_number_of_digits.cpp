b647733b6e168296c2c95c9b654bf65502fdb06a
mirinta <mirinta@163.com>
2025-04-30 10:25:27 +0800

    Update 1295_find_numbers_with_even_number_of_digits.cpp

--- array/1295_find_numbers_with_even_number_of_digits.cpp
+++ array/1295_find_numbers_with_even_number_of_digits.cpp
@@ -2,33 +2,34 @@
 
 /**
  * Given an array "nums" of integers, return how many of them contain an even number of digits.
+ *
+ * ! 1 <= nums.length <= 500
+ * ! 1 <= nums[i] <= 10^5
  */
 
 class Solution
 {
 public:
-    int findNumbers(const std::vector<int>& nums)
+    int findNumbers(std::vector<int>& nums)
     {
-        if (nums.empty())
-            return 0;
-
         int result = 0;
-        for (const auto& i : nums) {
-            if (digits(i) % 2 == 0) {
-                result++;
-            }
+        for (const auto& val : nums) {
+            result += isValid(val);
         }
         return result;
     }
 
 private:
-    int digits(int val)
+    bool isValid(int n)
     {
-        int count = 1;
-        while (val / 10) {
+        if (n == 0)
+            return false;
+
+        int count = 0;
+        while (n) {
             count++;
-            val /= 10;
+            n /= 10;
         }
-        return count;
+        return count % 2 == 0;
     }
 };
