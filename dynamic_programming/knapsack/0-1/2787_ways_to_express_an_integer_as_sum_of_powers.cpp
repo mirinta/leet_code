af305bd7b64212cb1ace1eb5b92b588ecdaf55c9
mirinta <mirinta@163.com>
2025-08-12 23:34:19 +0800

    2787

--- dynamic_programming/knapsack/0-1/2787_ways_to_express_an_integer_as_sum_of_powers.cpp
+++ dynamic_programming/knapsack/0-1/2787_ways_to_express_an_integer_as_sum_of_powers.cpp
@@ -8,7 +8,7 @@
  * integers, in other words, the number of sets of unique integers [n1, n2, ..., nk] where n = n1^x
  * + n2^x + ... + nk^x.
  *
- * Since the result can be very large, return it modulo 109 + 7.
+ * Since the result can be very large, return it modulo 10^9 + 7.
  *
  * For example, if n = 160 and x = 3, one way to express n is n = 23 + 33 + 53.
  *
@@ -22,18 +22,16 @@ public:
     int numberOfWays(int n, int x) { return approach2(n, x); }
 
 private:
-    static constexpr int kMod = 1e9 + 7;
+    static constexpr long long kMod = 1e9 + 7;
 
     int approach2(int n, int x)
     {
         std::vector<int> dp(n + 1, 0);
         dp[0] = 1;
         for (int i = 1; i <= n; ++i) {
-            const auto pow = std::pow(i, x);
-            for (int j = n; j >= 1; --j) {
-                if (j >= pow) {
-                    dp[j] = (dp[j] + dp[j - pow]) % kMod;
-                }
+            const long long pow = std::pow(i, x);
+            for (int j = n; j >= pow; --j) {
+                dp[j] = (dp[j] + dp[j - pow]) % kMod;
             }
         }
         return dp[n];
@@ -41,13 +39,13 @@ private:
 
     int approach1(int n, int x)
     {
-        // dp[i][j] = num of ways to make up j using [1:i-1]
+        // dp[i][j] = num of unique ways to make up j using integer [1:i]
         std::vector<std::vector<int>> dp(n + 1, std::vector<int>(n + 1, 0));
         for (int i = 0; i <= n; ++i) {
             dp[i][0] = 1;
         }
         for (int i = 1; i <= n; ++i) {
-            const auto pow = std::pow(i, x);
+            const long long pow = std::pow(i, x);
             for (int j = 1; j <= n; ++j) {
                 dp[i][j] = dp[i - 1][j];
                 if (j >= pow) {
