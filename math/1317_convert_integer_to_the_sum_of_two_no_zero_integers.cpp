f21f19f723c65184efc24a3414296f88d338201d
mirinta <mirinta@163.com>
2025-09-08 10:07:56 +0800

    Create 1317_convert_integer_to_the_sum_of_two_no_zero_integers.cpp

---
+++ math/1317_convert_integer_to_the_sum_of_two_no_zero_integers.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+
+/**
+ * No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+ *
+ * Given an integer n, return a list of two integers [a, b] where:
+ *
+ * - a and b are No-Zero integers.
+ *
+ * - a + b = n
+ *
+ * The test cases are generated so that there is at least one valid solution. If there are many
+ * valid solutions, you can return any of them.
+ *
+ * ! 2 <= n <= 10^4
+ */
+
+class Solution
+{
+public:
+    std::vector<int> getNoZeroIntegers(int n)
+    {
+        for (int a = 1; a <= n / 2; ++a) {
+            if (isValid(a) && isValid(n - a))
+                return {a, n - a};
+        }
+        return {};
+    }
+
+private:
+    bool isValid(int val)
+    {
+        if (val == 0)
+            return false;
+
+        while (val) {
+            if (val % 10 == 0)
+                return false;
+
+            val /= 10;
+        }
+        return true;
+    }
+};
