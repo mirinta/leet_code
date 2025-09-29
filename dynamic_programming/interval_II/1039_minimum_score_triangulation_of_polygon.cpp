2c0477e5844541504721bf0d7f22aa7d001fcb53
mirinta <mirinta@163.com>
2025-09-29 21:03:23 +0800

    Update 1039_minimum_score_triangulation_of_polygon.cpp

--- dynamic_programming/interval_II/1039_minimum_score_triangulation_of_polygon.cpp
+++ dynamic_programming/interval_II/1039_minimum_score_triangulation_of_polygon.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 #include <vector>
 
 /**
@@ -21,42 +22,26 @@ class Solution
 public:
     int minScoreTriangulation(std::vector<int>& values)
     {
-        /**
-         * indices are increasing in clockwise order
-         *
-         *   k  X
-         * X      X
-         *   i  j
-         *
-         * dp[i][j] = min score of triangulating the polygon i->...->k->...->j
-         * given i and j, for each k in the range [i+1,j-1]:
-         * - #NOTE# [i+1,j-1] must have at least one element, i.e., j-i>=2
-         * - we have one triangle and two polygons
-         * - i-k-j is a triangle with score T = val[i]*val[j]*val[k]
-         * - i->...->k is a polygon, min score of solving this subproblem =  dp[i][k]
-         * - k->...->j is another polygon, min score of solving this subproblem = dp[k][j]
-         * - thus, dp[i][j] = min(T + dp[i][k] + dp[k][j]), where k is in the range [i+1,j-1]
-         */
         const int n = values.size();
-        std::vector<std::vector<int>> dp(n, std::vector<int>(n, INT_MAX));
-        // base case:
-        // - if j-i<2, dp[i][j] = 0, because there's no possible k in the range [i+1,j-1]
-        // - otherwise, dp[i][j] = INT_MAX
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                if (j - i < 2) {
-                    dp[i][j] = 0;
-                }
-            }
+        std::vector<std::vector<int>> memo(n, std::vector<int>(n, -1));
+        return dfs(memo, 0, values.size() - 1, values);
+    }
+
+private:
+    // min score of triangulating polygon[i:j]
+    int dfs(std::vector<std::vector<int>>& memo, int i, int j, const std::vector<int>& values)
+    {
+        if (j - i < 2)
+            return 0;
+
+        if (memo[i][j] != -1)
+            return memo[i][j];
+
+        int result = INT_MAX;
+        for (int k = i + 1; k < j; ++k) {
+            const int score = values[i] * values[j] * values[k];
+            result = std::min(result, dfs(memo, i, k, values) + dfs(memo, k, j, values) + score);
         }
-        for (int i = n - 3; i >= 0; --i) {
-            for (int j = i + 2; j < n; ++j) {
-                for (int k = i + 1; k <= j - 1; ++k) {
-                    const auto product = values[i] * values[j] * values[k];
-                    dp[i][j] = std::min(dp[i][j], product + dp[i][k] + dp[k][j]);
-                }
-            }
-        }
-        return dp[0][n - 1];
+        return memo[i][j] = result;
     }
 };
