d60fc1388eaa105c89a36f4f041596953b875635
mirinta <mirinta@163.com>
2025-10-30 00:49:19 +0800

    3370

---
+++ bit_manipulation/3370_smallest_number_with_all_set_bits.cpp
@@ -0,0 +1,25 @@
+/**
+ * You are given a positive number n.
+ *
+ * Return the smallest number x greater than or equal to n, such that the binary representation of x
+ * contains only set bits.
+ *
+ * ! 1 <= n <= 1000
+ */
+
+class Solution
+{
+public:
+    int smallestNumber(int n) { return (1 << numOfBits(n)) - 1; }
+
+private:
+    int numOfBits(int n)
+    {
+        int result = 0;
+        while (n) {
+            result++;
+            n >>= 1;
+        }
+        return result;
+    }
+};
