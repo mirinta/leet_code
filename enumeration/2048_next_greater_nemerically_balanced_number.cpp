48ac148e73d25f47bb99bc8348e0d606d2bbe14e
mirinta <mirinta@163.com>
2025-10-31 00:21:57 +0800

    2048

---
+++ enumeration/2048_next_greater_nemerically_balanced_number.cpp
@@ -0,0 +1,38 @@
+#include <array>
+
+/**
+ * An integer x is numerically balanced if for every digit d in the number x, there are exactly d
+ * occurrences of that digit in x.
+ *
+ * Given an integer n, return the smallest numerically balanced number strictly greater than n.
+ *
+ * ! 0 <= n <= 10^6
+ */
+
+class Solution
+{
+public:
+    int nextBeautifulNumber(int n)
+    {
+        for (int i = n + 1; i <= 1224444; ++i) {
+            if (isValid(i))
+                return i;
+        }
+        return -1;
+    }
+
+private:
+    bool isValid(int n)
+    {
+        std::array<int, 10> count{};
+        while (n) {
+            count[n % 10]++;
+            n /= 10;
+        }
+        for (int i = 0; i < 10; ++i) {
+            if (count[i] > 0 && count[i] != i)
+                return false;
+        }
+        return true;
+    }
+};
