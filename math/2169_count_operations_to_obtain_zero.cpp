bc42a6498a3fa871b51201757d3147dab62ab8bf
mirinta <mirinta@163.com>
2025-11-10 01:50:51 +0800

    2169

---
+++ math/2169_count_operations_to_obtain_zero.cpp
@@ -0,0 +1,33 @@
+#include <utility>
+
+/**
+ * You are given two non-negative integers num1 and num2.
+ *
+ * In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from
+ * num2.
+ *
+ * For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2
+ * = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
+ *
+ * Return the number of operations required to make either num1 = 0 or num2 = 0.
+ *
+ * ! 0 <= num1, num2 <= 10^5
+ */
+
+class Solution
+{
+public:
+    int countOperations(int num1, int num2)
+    {
+        if (num1 < num2)
+            return countOperations(num2, num1);
+
+        int result = 0;
+        while (num1 != 0 && num2 != 0) {
+            result += num1 / num2;
+            num1 %= num2;
+            std::swap(num1, num2);
+        }
+        return result;
+    }
+};
