88eda8b0bf3321bb1662d2b312267fca96826d3c
mirinta <mirinta@163.com>
2025-11-23 16:26:44 +0800

    1262

---
+++ dynamic_programming/1262_greatest_sum_divisible_by_three.cpp
@@ -0,0 +1,28 @@
+#include <array>
+#include <vector>
+
+/**
+ * Given an integer array nums, return the maximum possible sum of elements of the array such that
+ * it is divisible by three.
+ *
+ * ! 1 <= nums.length <= 4 * 10^4
+ * ! 1 <= nums[i] <= 10^4
+ */
+
+class Solution
+{
+public:
+    int maxSumDivThree(std::vector<int>& nums)
+    {
+        // dp[i][j] = maximum sum of nums[0:i-1] that the sum % 3 = j
+        const int n = nums.size();
+        std::vector<std::array<int, 3>> dp(n + 1, {0, INT_MIN, INT_MIN});
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                const int target = (j - nums[i - 1] % 3 + 3) % 3;
+                dp[i][j] = std::max(dp[i - 1][j], nums[i - 1] + dp[i - 1][target]);
+            }
+        }
+        return dp[n][0];
+    }
+};
