60bbb5fcc3662b59e401e8f8444edcb3e9ade426
mirinta <mirinta@163.com>
2025-11-30 02:01:04 +0800

    3512

---
+++ array/3512_minimum_operations_to_make_array_sum_divisible_by_k.cpp
@@ -0,0 +1,25 @@
+#include <numeric>
+#include <vector>
+
+/**
+ * You are given an integer array nums and an integer k. You can perform the following operation any
+ * number of times:
+ *
+ * - Select an index i and replace nums[i] with nums[i] - 1.
+ *
+ * Return the minimum number of operations required to make the sum of the array divisible by k.
+ *
+ * ! 1 <= nums.length <= 1000
+ * ! 1 <= nums[i] <= 1000
+ * ! 1 <= k <= 100
+ */
+
+class Solution
+{
+public:
+    int minOperations(std::vector<int>& nums, int k)
+    {
+        const int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        return sum % k;
+    }
+};
