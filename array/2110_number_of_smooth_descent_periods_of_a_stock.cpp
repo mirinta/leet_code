fc9c8ace78e41c3411630b6c22b49ba52f7f2902
mirinta <mirinta@163.com>
2025-12-15 14:12:12 +0800

    2110

---
+++ array/2110_number_of_smooth_descent_periods_of_a_stock.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+/**
+ * You are given an integer array prices representing the daily price history of a stock, where
+ * prices[i] is the stock price on the ith day.
+ *
+ * A smooth descent period of a stock consists of one or more contiguous days such that the price on
+ * each day is lower than the price on the preceding day by exactly 1. The first day of the period
+ * is exempted from this rule.
+ *
+ * Return the number of smooth descent periods.
+ *
+ * ! 1 <= prices.length <= 10^5
+ * ! 1 <= prices[i] <= 10^5
+ */
+
+class Solution
+{
+public:
+    long long getDescentPeriods(std::vector<int>& prices)
+    {
+        const int n = prices.size();
+        long long result = 0;
+        int i = 0;
+        while (i < n) {
+            int j = i + 1;
+            while (j < n && prices[j - 1] - prices[j] == 1) {
+                j++;
+            }
+            result += 1LL * (j - i + 1) * (j - i) / 2;
+            i = j;
+        }
+        return result;
+    }
+};
