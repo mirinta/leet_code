d8805905abbb95a45ba41020fd1091da6d60a55e
mirinta <mirinta@163.com>
2025-12-26 23:05:57 +0800

    2483

--- array/2483_minimum_penalty_for_a_shop.cpp
+++ array/2483_minimum_penalty_for_a_shop.cpp
@@ -29,55 +29,44 @@ public:
     int bestClosingTime(std::string customers) { return approach2(customers); }
 
 private:
-    // One-pass, time O(N), space O(1)
     int approach2(const std::string& customers)
     {
         const int n = customers.size();
+        int min = 0;
         int result = 0;
-        int penalty = 0;
-        int minPenalty = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0, penalty = 0; i < n; ++i) {
             if (customers[i] == 'Y') {
                 penalty--;
             } else {
                 penalty++;
             }
-            if (penalty < minPenalty) {
+            if (penalty < min) {
+                min = penalty;
                 result = i + 1;
-                minPenalty = penalty;
             }
         }
         return result;
     }
 
-    // Three-pass, time O(N), space O(N)
     int approach1(const std::string& customers)
     {
-        // X X X X X X j X X X X X X
-        // |<-OPEN-->| |<--CLOSED->|
+        // penalty += 1 for each N in [0:j-1]
+        // penalty += 1 for each Y in [j+1:n-1]
         const int n = customers.size();
-        std::vector<int> prefix(n + 1, 0);
+        std::vector<std::pair<int, int>> presum(n + 1, {0, 0});
         for (int i = 1; i <= n; ++i) {
-            if (customers[i - 1] == 'N') {
-                prefix[i] = prefix[i - 1] + 1;
-            } else {
-                prefix[i] = prefix[i - 1];
-            }
+            presum[i].first = presum[i - 1].first + (customers[i - 1] == 'Y');
+            presum[i].second = presum[i - 1].second + (customers[i - 1] == 'N');
         }
-        std::vector<int> suffix(n + 1, 0);
-        for (int i = n - 1; i >= 0; --i) {
-            if (customers[i] == 'Y') {
-                suffix[i] = suffix[i + 1] + 1;
-            } else {
-                suffix[i] = suffix[i + 1];
-            }
-        }
-        int penalty = INT_MAX;
-        int result = -1;
-        for (int i = 0; i <= n; ++i) {
-            if (prefix[i] + suffix[i] < penalty) {
-                result = i;
-                penalty = prefix[i] + suffix[i];
+        int min = presum[n].second;
+        int result = n;
+        for (int j = 0; j < n; ++j) {
+            const int penalty = presum[j].second + presum[n].first - presum[j].first;
+            if (penalty < min) {
+                min = penalty;
+                result = j;
+            } else if (penalty == min) {
+                result = std::min(result, j);
             }
         }
         return result;
