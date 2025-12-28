bf0cde39184712ecd595f36f0bcd641df68c2017
mirinta <mirinta@163.com>
2025-12-28 13:04:54 +0800

    1351

--- binary_search/1351_count_negative_numbers_in_a_sorted_matrix.cpp
+++ binary_search/1351_count_negative_numbers_in_a_sorted_matrix.cpp
@@ -14,58 +14,22 @@
 class Solution
 {
 public:
-    int countNegatives(std::vector<std::vector<int>>& grid) { return approach2(grid); }
-
-private:
-    // approach2: time O(mlogn), space O(1)
-    int approach2(std::vector<std::vector<int>>& grid)
+    int countNegatives(std::vector<std::vector<int>>& grid)
     {
-        if (grid.empty())
-            return 0;
-
+        // always check the top-right element grid[i][j]
+        // if grid[i][j] >= 0, then the ith row can be skipped
+        // otherwise, the grid[i:m-1][j] are all negative numbers
         const int m = grid.size();
         const int n = grid[0].size();
-        if (grid[m - 1][n - 1] >= 0)
-            return 0;
-
+        int i = 0;
+        int j = n - 1;
         int result = 0;
-        for (const auto& row : grid) {
-            const int index = std::distance(
-                row.begin(), std::upper_bound(row.begin(), row.end(), 0, std::greater<int>{}));
-            result += (n - index);
-        }
-        return result;
-    }
-
-    // approach1: time O(m+n), space O(1)
-    int approach1(std::vector<std::vector<int>>& grid)
-    {
-        if (grid.empty())
-            return 0;
-
-        //  4  3  2 -1
-        //  3  2  1 -1
-        //  1  1 -1 -2
-        // -1 -1 -2 -3
-        const int m = grid.size();
-        const int n = grid[0].size();
-        // top-left is the largest element
-        // bottom-right is the smallest element
-        if (grid[m - 1][n - 1] >= 0)
-            return 0;
-
-        // always check top-right: grid[x][y]
-        // - it is the largest of column y
-        // - it is the smallest of row x
-        int result = 0;
-        int row = 0;
-        int col = n - 1;
-        while (row < m && col >= 0) {
-            if (grid[row][col] < 0) {
-                result += m - row;
-                col--;
+        while (i < m && j >= 0) {
+            if (grid[i][j] >= 0) {
+                i++;
             } else {
-                row++;
+                result += m - i;
+                j--;
             }
         }
         return result;
