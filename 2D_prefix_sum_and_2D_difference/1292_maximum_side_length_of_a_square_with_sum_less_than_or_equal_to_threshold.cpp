3b393977838c3b0bef91a5a6ddbd39dc8e21405e
mirinta <mirinta@163.com>
2026-01-19 22:51:25 +0800

    Update 1292_maximum_side_length_of_a_square_with_sum_less_than_or_equal_to_threshold.cpp

--- 2D_prefix_sum_and_2D_difference/1292_maximum_side_length_of_a_square_with_sum_less_than_or_equal_to_threshold.cpp
+++ 2D_prefix_sum_and_2D_difference/1292_maximum_side_length_of_a_square_with_sum_less_than_or_equal_to_threshold.cpp
@@ -11,46 +11,53 @@
  * ! 0 <= threshold <= 10^5
  */
 
-class Solution {
+class Presum2D {
 public:
-    int maxSideLength(std::vector<std::vector<int>>& mat, int threshold)
+    explicit Presum2D(const std::vector<std::vector<int>>& mat)
+        : presum(mat.size() + 1, std::vector<int>(mat[0].size() + 1, 0))
     {
-        const int m = mat.size();
-        const int n = mat[0].size();
-        // presum[i][j] = sum of all elements in matrix[0:i-1][0:j-1]
-        std::vector<std::vector<long long>> presum(m + 1, std::vector<long long>(n + 1, 0));
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                presum[i][j] = presum[i - 1][j] + presum[i][j - 1] - presum[i - 1][j - 1] + mat[i - 1][j - 1];
+        for (int i = 1; i <= mat.size(); ++i) {
+            for (int j = 1; j <= mat[0].size(); ++j) {
+                presum[i][j] = mat[i - 1][j - 1] + presum[i - 1][j] + presum[i][j - 1] - presum[i - 1][j - 1];
             }
         }
-        // guess the final answer
-        int lo = 0;
-        int hi = std::min(m, n);
-        while (lo < hi) {
-            const int length = hi - (hi - lo) / 2; // length of the square
-            if (isValid(length, m, n, presum, threshold)) {
-                lo = length;
-            } else {
-                hi = length - 1;
-            }
-        }
-        return lo;
     }
 
-private:
-    // return sum of all elements in matrix[x1:x2][y1:y2]
-    long long query(int x1, int y1, int x2, int y2, const std::vector<std::vector<long long>>& presum)
+    int query(int x1, int y1, int x2, int y2) const
     {
         return presum[x2 + 1][y2 + 1] - presum[x2 + 1][y1] - presum[x1][y2 + 1] + presum[x1][y1];
     }
 
-    // check if there exists a square of given length that sum of all its elements <= threshold
-    bool isValid(int length, int m, int n, const std::vector<std::vector<long long>>& presum, long long threshold)
+private:
+    std::vector<std::vector<int>> presum;
+};
+
+class Solution {
+public:
+    int maxSideLength(std::vector<std::vector<int>>& mat, int threshold)
     {
-        for (int i = 0; i + length - 1 < m; ++i) {
-            for (int j = 0; j + length - 1 < n; ++j) {
-                if (query(i, j, i + length - 1, j + length - 1, presum) <= threshold)
+        Presum2D presum(mat);
+        const int m = mat.size();
+        const int n = mat[0].size();
+        int lo = 0;
+        int hi = std::min(m, n);
+        while (lo < hi) {
+            const int mid = hi - (hi - lo) / 2;
+            if (isValid(m, n, mid, threshold, presum)) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return hi;
+    }
+
+private:
+    bool isValid(int m, int n, int length, int threshold, const Presum2D& presum)
+    {
+        for (int i = 0; i + length <= m; ++i) {
+            for (int j = 0; j + length <= n; ++j) {
+                if (presum.query(i, j, i + length - 1, j + length - 1) <= threshold)
                     return true;
             }
         }
