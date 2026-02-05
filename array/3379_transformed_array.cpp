ad91372f1c9c60112fb48ec127ce363a18134e1e
mirinta <mirinta@163.com>
2026-02-05 15:12:44 +0800

    Create 3379_transformed_array.cpp

---
+++ array/3379_transformed_array.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+
+/**
+ * You are given an integer array nums that represents a circular array. Your task is to create a new array result of
+ * the same size, following these rules:
+ *
+ * For each index i (where 0 <= i < nums.length), perform the following independent actions:
+ *
+ * - If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the
+ * value of the index where you land.
+ *
+ * - If nums[i] < 0: Start at index i and move abs(nums[i]) steps to the left in the circular array. Set result[i] to
+ * the value of the index where you land.
+ *
+ * - If nums[i] == 0: Set result[i] to nums[i].
+ *
+ * Return the new array result.
+ *
+ * Note: Since nums is circular, moving past the last element wraps around to the beginning, and moving before the first
+ * element wraps back to the end.
+ *
+ * ! 1 <= nums.length <= 100
+ * ! -100 <= nums[i] <= 100
+ */
+
+class Solution {
+public:
+    std::vector<int> constructTransformedArray(std::vector<int>& nums)
+    {
+        const int n = nums.size();
+        std::vector<int> result;
+        result.reserve(n);
+        for (int i = 0; i < n; ++i) {
+            const int steps = std::abs(nums[i]) % n;
+            if (nums[i] > 0) {
+                result.push_back(nums[(i + steps) % n]);
+            } else if (nums[i] < 0) {
+                result.push_back(nums[(i - steps + n) % n]);
+            } else {
+                result.push_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
