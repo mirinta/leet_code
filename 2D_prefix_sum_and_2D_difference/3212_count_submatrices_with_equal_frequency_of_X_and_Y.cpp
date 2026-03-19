42490ad531e6e29b295073044d2a7710bb27c63a
mirinta <mirinta@163.com>
2026-03-19 11:45:09 +0800

    3212

--- 2D_prefix_sum_and_2D_difference/3212_count_submatrices_with_equal_frequency_of_X_and_Y.cpp
+++ 2D_prefix_sum_and_2D_difference/3212_count_submatrices_with_equal_frequency_of_X_and_Y.cpp
@@ -14,43 +14,21 @@
  * ! grid[i][j] is either 'X', 'Y', or '.'.
  */
 
-class Prefix2D {
-public:
-    Prefix2D(char c, int m, int n, const std::vector<std::vector<char>>& grid)
-        : presum(m + 1, std::vector<int>(n + 1, 0))
-    {
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                presum[i][j] = presum[i][j - 1] + presum[i - 1][j] - presum[i - 1][j - 1] + (grid[i - 1][j - 1] == c);
-            }
-        }
-    }
-
-    int query(int x1, int y1, int x2, int y2) const
-    {
-        return presum[x2 + 1][y2 + 1] - presum[x2 + 1][y1] - presum[x1][y2 + 1] + presum[x1][y1];
-    }
-
-private:
-    std::vector<std::vector<int>> presum;
-};
-
 class Solution {
 public:
     int numberOfSubmatrices(std::vector<std::vector<char>>& grid)
     {
         const int m = grid.size();
         const int n = grid[0].size();
-        Prefix2D prefix2DX('X', m, n, grid);
-        Prefix2D prefix2DY('Y', m, n, grid);
+        std::vector<std::vector<std::pair<int, int>>> presum(m + 1, std::vector<std::pair<int, int>>(n + 1, {0, 0}));
         int result = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                const int countX = prefix2DX.query(0, 0, i, j);
-                const int countY = prefix2DY.query(0, 0, i, j);
-                if (countX >= 1 && countX == countY) {
-                    result++;
-                }
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                presum[i][j].first = presum[i - 1][j].first + presum[i][j - 1].first + (grid[i - 1][j - 1] == 'X') -
+                                     presum[i - 1][j - 1].first;
+                presum[i][j].second = presum[i - 1][j].second + presum[i][j - 1].second + (grid[i - 1][j - 1] == 'Y') -
+                                      presum[i - 1][j - 1].second;
+                result += presum[i][j].first > 0 && presum[i][j].first == presum[i][j].second;
             }
         }
         return result;
