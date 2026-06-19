d685199ea3149aa1e560e899970df03fa837b5fa
mirinta <mirinta@163.com>
2026-06-19 20:41:20 +0800

    1259

--- dynamic_programming/1259_handshakes_that_don't_cross.cpp
+++ dynamic_programming/1259_handshakes_that_don't_cross.cpp
@@ -17,14 +17,18 @@ public:
     int numberOfWays(int numPeople)
     {
         // dp[i] = num of ways to shake hands of i people
-        // let person 1 shakes hands with person j,
-        // then the circle is divided into two parts,
-        // right part has j-2 people and left part has i-j people
-        //       1
-        //  i    |    2
-        // ...   |   ...
-        // x+1   |   j+1
-        //       j
+        //
+        // let people 1 and j shake hands, where j = 2, 4, 6, ...
+        //
+        //   2 ... j-1
+        // 1 --------- j
+        //   i ... j+1
+        //
+        // the circle is divided in two parts:
+        // - part 1: num of people = j-2
+        // - part 2: num of people = i-j
+        //
+        // then, dp[i] = sum of (dp[i-j] * dp[j-2])
         constexpr int kMod = 1e9 + 7;
         std::vector<long long> dp(numPeople + 1);
         dp[0] = 1;
