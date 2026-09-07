f09898e50ed8e1b055a0253b6b8321cd00ca72d7
mirinta <mirinta@163.com>
2026-09-07 19:13:21 +0800

    Update 0115_distinct_subsequences.cpp

--- dynamic_programming/double_sequences/0115_distinct_subsequences.cpp
+++ dynamic_programming/double_sequences/0115_distinct_subsequences.cpp
@@ -12,24 +12,23 @@
 
 class Solution {
 public:
-    int numDistinct(std::string s, std::string t)
+    int numDistinct(std::string& s, std::string& t)
     {
         return approach2(s, t);
     }
 
 private:
+    using ULL = unsigned long long;
+
     int approach2(const std::string& s, const std::string& t)
     {
         const int m = s.size();
         const int n = t.size();
-        if (m < n)
-            return 0;
-
-        std::vector<size_t> dp(n + 1, 0);
+        std::vector<ULL> dp(n + 1, 0);
         dp[0] = 1;
-        for (const auto& c : s) {
-            for (int j = n; j >= 1; --j) {
-                if (c == t[j - 1]) {
+        for (int i = 1; i <= m; ++i) {
+            for (int j = std::min(i, n); j >= 1; --j) {
+                if (s[i - 1] == t[j - 1]) {
                     dp[j] += dp[j - 1];
                 }
             }
@@ -39,18 +38,15 @@ private:
 
     int approach1(const std::string& s, const std::string& t)
     {
-        // dp[i][j] = num of distinct subsequences of s[0:i-1] which equals t[0:j-1]
+        // dp[i][j] = num of subsequences of s[0:i-1] which equals t[0:j-1]
         const int m = s.size();
         const int n = t.size();
-        if (m < n)
-            return 0;
-
-        std::vector<std::vector<size_t>> dp(m + 1, std::vector<size_t>(n + 1, 0));
+        std::vector dp(m + 1, std::vector<ULL>(n + 1, 0));
         for (int i = 0; i <= m; ++i) {
             dp[i][0] = 1;
         }
         for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
+            for (int j = 1; j <= std::min(i, n); ++j) {
                 dp[i][j] = dp[i - 1][j];
                 if (s[i - 1] == t[j - 1]) {
                     dp[i][j] += dp[i - 1][j - 1];
